186b3fa06e00bba9c3ce295b989c685b2fddd48d
zhong kang ping <15979624097@163.com>
2026-06-01 20:57:36 +0800

    删除多余页面，只保留一个页面

--- sidebars.ts
+++ sidebars.ts
@@ -2,7 +2,6 @@ import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
 
 const sidebars: SidebarsConfig = {
   tutorialSidebar: [
-    'intro',
     {
       type: 'category',
       label: '嵌入式',
