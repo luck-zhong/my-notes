bfed7af9afa83e5edbcab33c4e46776c85b0a32d
zhong kang ping <15979624097@163.com>
2026-06-03 20:52:35 +0800

    Redirect home to first docs page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import {Redirect} from '@docusaurus/router';
-import useBaseUrl from '@docusaurus/useBaseUrl';
+import {useLatestVersion} from '@docusaurus/plugin-content-docs/client';
 
 export default function Home(): React.ReactNode {
-  return <Redirect to={useBaseUrl('/embedded/stm32')} />;
+  const docsVersion = useLatestVersion(undefined);
+  const mainDoc =
+    docsVersion.docs.find((doc) => doc.id === docsVersion.mainDocId) ??
+    docsVersion.docs[0];
+
+  return <Redirect to={mainDoc.path} />;
 }
